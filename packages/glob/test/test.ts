2d6d8bf60bc0b9ae5cc5ea68f6544c9ea2fb8792
refirst11 <ocelot2828@gmail.com>
2025-05-29 05:55:14 +0900

    feat(test): update glob tests add absolute pattern

--- packages/glob/test/test.ts
+++ packages/glob/test/test.ts
@@ -1,17 +1,16 @@
-import { cwd } from "process";
-
+const path = require("path");
 const test = require("node:test");
 const assert = require("node:assert");
 const { globSync, glob } = require("../index");
 
 test("async glob", async () => {
-  const files = await glob("**/*.rs", { cwd: "src" });
+  const files = await glob(path.join(process.cwd()), "src/**/*.rs");
   console.log(files);
   assert(Array.isArray(files));
 });
 
 test("sync glob", () => {
-  const files = globSync("src/**/*.rs", { cwd: cwd() });
+  const files = globSync("src/**/*.rs");
   console.log(files);
   assert(Array.isArray(files));
 });
@@ -19,7 +18,6 @@ test("sync glob", () => {
 test("glob with exclude", () => {
   const files = globSync("**/*.rs", {
     exclude: ["**/test/**", "**/target/**"],
-    cwd: cwd(),
   });
   console.log(files);
   assert(Array.isArray(files));
